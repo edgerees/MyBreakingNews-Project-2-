b794a0b85c8d5ad7b0ab20decf8b7c8b06b2d1b4
Edree Huss <edreeshuss@gmail.com>
2020-12-02 14:47:16 -0500

    [news.ejs] Get articles to display.

--- routes/news.js
+++ routes/news.js
@@ -7,19 +7,24 @@ const API_KEY = process.env.API_KEY;
 router.get('/', async(req, res) => {
    
     try {
-        const newsAPI = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
-        
-        console.log(newsAPI.data)
-
- res.render('partials/news')
-
-    
-
-
-    } catch (error) {
-
-
-    }
-    })
+        const newsAPI = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)       
+        // console.log(newsAPI.data)
+        res.render('partials/news', {articles: newsAPI.data.articles})
+    } catch (err) {
+        if(err.response) {
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+            res.render('news', { articles : null })
+        } else if(err.requiest) {
+            res.render('news', { articles : null })
+            console.log(err.requiest)
+        } else {
+            res.render('news', { articles : null })
+            console.error('Error', err.message)
+        }
+    } 
+})
 
 module.exports = router;
+
--- server.js
+++ server.js
@@ -12,21 +12,25 @@ const isLoggedIn = require('./middleware/isLoggedIn');
 
 app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
+app.use('/css', express.static(__dirname +'public/css'));
+app.use('/img', express.static(__dirname +'public/img'));
+app.use('/js', express.static(__dirname +'public/js'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
-app.use('/img', express.static(__dirname + '/public/img'));
-app.use('/js', express.static(__dirname + '/public/js'));
+app.use(layouts);
 
-// app.use(layouts);
 // secret: What we actually will be giving the user on our site as a session cookie
 // resave: Save the session even if it's modified, make this false
 // saveUninitialized: If we have a new session, we save it, therefore making that true
+
 const sessionObject = {
   secret: SECRET_SESSION,
   resave: false,
   saveUninitialized: true
 }
+
 app.use(session(sessionObject));
+
 // Initialize passport and run through middleware
 app.use(passport.initialize());
 app.use(passport.session());
@@ -53,22 +57,14 @@ app.get('/profile', isLoggedIn, (req, res) => {
 });
 
 
-
-
-const newsRouter = require('./routes/news');
-app.use ('/news', newsRouter) 
-"app.use('/news', require('./routes/news'))"
-
-
-
-const router = require('./routes/auth');
 app.use('/auth', require('./routes/auth'));
+app.use('/news', require('./routes/news'))
 
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
-  console.log(`You're listening to the smooth sounds of port ${PORT}`);
+  console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`);
 });
 
+
 module.exports = server;
-module.exports = router;
